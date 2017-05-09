1b9f3a50b08e2afe29107ac03d02bb3fb04543c3
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-05-09 09:51:30 -0500

    added complex password

--- examples/azure-web-app-redis-cache-sql-database/deploy.mac.sh
+++ examples/azure-web-app-redis-cache-sql-database/deploy.mac.sh
@@ -6,7 +6,8 @@ if docker -v; then
 
   # generate a unique string for CI deployment
   export KEY=$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z' | head -c 12)
-  export PASSWORD=$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
+  export PASSWORD=$#@abcd9753w0w@h@
+  # =$KEY$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'A-Z' | head -c 2)$(cat /dev/urandom | env LC_CTYPE=C tr -cd '0-9' | head -c 2)
 
 /bin/sh ./deploy.ci.sh
 
