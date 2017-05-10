29b743e4ab202213d41957ff81f81ab9746e4cb0
Scott Nowicki <scott@sgn-mbp>
2017-05-10 09:34:54 -0500

    removed leftover code form install consolidation

--- examples/azure-sql-database-tranparent-encryption-create/deploy.ci.sh
+++ examples/azure-sql-database-tranparent-encryption-create/deploy.ci.sh
@@ -1,35 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 set -o errexit -o nounset
 
-TERRAFORM_VERSION=0.9.4
-TERRAFORM_SHA256SUM=cc1cffee3b82820b7f049bb290b841762ee920aef3cf4d95382cc7ea01135707
-
-apt update
-apt install -y --no-install-recommends git curl openssh-client unzip apt-utils ca-certificates apt-transport-https
-
-# Import the public repository GPG keys for Microsoft
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-
-# Register the Microsoft Ubuntu 14.04 repository
-curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | tee /etc/apt/sources.list.d/microsoft.list
-
-# Install powershell from Microsoft Repo
-apt-get update
-apt-get install -y --no-install-recommends powershell
-
-# install terraform
-curl "https://releases.hashicorp.com/terraform/"+=$TERRAFORM_VERSION+="/terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip" > terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-echo $TERRAFORM_SHA256SUM+="  terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip" > "terraform_"+=$TERRAFORM_VERSION+="_SHA256SUMS"
-sha256sum -cs "terraform_"+=$TERRAFORM_VERSION+="_SHA256SUMS"
-unzip "terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip" -d /bin
-rm -f "terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip"
-
-/bin/terraform get
-/bin/terraform validate
-/bin/terraform plan -out=out.tfplan -var resource_group=$KEY -var sql_admin=$KEY -var sql_password=$PASSWORD
-/bin/terraform apply out.tfplan
-/bin/terraform show
+terraform get
+terraform validate
+terraform plan -out=out.tfplan -var resource_group=$KEY -var sql_admin=$KEY -var sql_password=$PASSWORD
+terraform apply out.tfplan
+terraform show
 
 # # check that resources exist via azure cli
 # docker run --rm -it \
