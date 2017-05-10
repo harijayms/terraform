561bc263639d8d74ece5f30d50dfcb0fbd58f0d1
Scott Nowicki <scott@sgn-mbp>
2017-05-10 09:56:42 -0500

    wrong option for sha256sum on ubuntu

--- examples/azure-sql-database-tranparent-encryption-create/install.ci.sh
+++ examples/azure-sql-database-tranparent-encryption-create/install.ci.sh
@@ -21,6 +21,6 @@ apt-get install -y --no-install-recommends powershell
 # install terraform
 curl "https://releases.hashicorp.com/terraform/"+=$TERRAFORM_VERSION+="/terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip" > terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 echo $TERRAFORM_SHA256SUM+="  terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip" > "terraform_"+=$TERRAFORM_VERSION+="_SHA256SUMS"
-sha256sum -cs "terraform_"+=$TERRAFORM_VERSION+="_SHA256SUMS"
+sha256sum -c --quiet "terraform_"+=$TERRAFORM_VERSION+="_SHA256SUMS"
 unzip "terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip" -d /bin
 rm -f "terraform_"+=$TERRAFORM_VERSION+="_linux_amd64.zip"
